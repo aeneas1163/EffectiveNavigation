05071f978767d86c9ce411066331f8ebb3ca59c2
Alphan <aeneas1163@gmail.com>
2014-12-28 21:35:44 +0100

    sending mail when someone donates is implemented :D

--- app/src/main/java/com/alphan/mcan/snoozecharity/viewModels/mainActiviy/PendingDonationListAdapter.java
+++ app/src/main/java/com/alphan/mcan/snoozecharity/viewModels/mainActiviy/PendingDonationListAdapter.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -19,7 +20,20 @@ import com.alphan.mcan.snoozecharity.R;
 import com.alphan.mcan.snoozecharity.data.model.PendingDonationDataModel;
 import com.alphan.mcan.snoozecharity.services.AlarmManagerHelper;
 
+import java.io.UnsupportedEncodingException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.List;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 /**
  * Created by Alphan on 26-Dec-14.
@@ -99,6 +113,7 @@ public class PendingDonationListAdapter extends ArrayAdapter<PendingDonationData
                                         AlarmManagerHelper.addPaidSMSDonation(mContext, model, currentSMSAmount);
                                         remove(model);
                                         notifyDataSetChanged();
+                                        sendMail(currentSMSAmount, charities[charityIndex]);
                                     }
                                 });
                                 areYouSureDialog.show();
@@ -116,6 +131,7 @@ public class PendingDonationListAdapter extends ArrayAdapter<PendingDonationData
                                     model.setPendingAmount(leftoverAmount);
                                 }
                                 notifyDataSetChanged();
+                                sendMail(currentSMSAmount, charities[charityIndex]);
                             }
 
                         }
@@ -123,6 +139,7 @@ public class PendingDonationListAdapter extends ArrayAdapter<PendingDonationData
                             AlarmManagerHelper.transferToPaidDonation(mContext, model);
                             remove(model);
                             notifyDataSetChanged();
+                            sendMail(model.getPendingAmount(), charities[charityIndex]);
                             String url = charityWebsites[model.getCharityIndex()];
                             Intent goToDonationWebsite = new Intent(Intent.ACTION_VIEW);
                             goToDonationWebsite.setData(Uri.parse(url));
@@ -135,5 +152,63 @@ public class PendingDonationListAdapter extends ArrayAdapter<PendingDonationData
         });
 
         return view;
+    }
+
+    private void sendMail(Double amount, String charity) {
+        Session session = createSessionObject();
+
+        Calendar calendar = Calendar.getInstance();
+
+        SimpleDateFormat df = new SimpleDateFormat("dd-MMM-yyyy");
+        String formattedDate = df.format(calendar.getTime());
+
+        String text = charity + " " + String.format("%.2f",amount) + " " + mContext.getString(R.string.money_sign) + " " + formattedDate;
+
+        try {
+            Message message = createMessage(mContext.getString(R.string.username), text, text, session);
+            new SendMailTask().execute(message);
+        } catch (AddressException e) {
+            e.printStackTrace();
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private Message createMessage(String email, String subject, String messageBody, Session session) throws MessagingException, UnsupportedEncodingException {
+        Message message = new MimeMessage(session);
+        message.setFrom(new InternetAddress(mContext.getString(R.string.username), mContext.getString(R.string.app_name)));
+        message.addRecipient(Message.RecipientType.TO, new InternetAddress(email, email));
+        message.setSubject(subject);
+        message.setText(messageBody);
+        return message;
+    }
+
+    private Session createSessionObject() {
+        Properties properties = new Properties();
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.smtp.host", "smtp.gmail.com");
+        properties.put("mail.smtp.port", "587");
+
+        return Session.getInstance(properties, new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(mContext.getString(R.string.username), mContext.getString(R.string.password));
+            }
+        });
+    }
+
+    private class SendMailTask extends AsyncTask<Message, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Message... messages) {
+            try {
+                Transport.send(messages[0]);
+            } catch (MessagingException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
     }
 }
