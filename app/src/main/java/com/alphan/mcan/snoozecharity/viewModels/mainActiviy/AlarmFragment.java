a12ab530c9018386ad5a9620a528de317ff19995
Can Isik <mmtcan@gmail.com>
2015-01-17 11:40:56 +0100

    ENHANCEMENT #23: modified the behaviour of toggle repeat buttons according to spec, also did some minot refactroring

--- app/src/main/java/com/alphan/mcan/snoozecharity/viewModels/mainActiviy/AlarmFragment.java
+++ app/src/main/java/com/alphan/mcan/snoozecharity/viewModels/mainActiviy/AlarmFragment.java
@@ -46,73 +46,83 @@ public class AlarmFragment extends Fragment{
 
         View rootView = inflater.inflate(R.layout.fragment_section_alarm, container, false);
 
-        View background = rootView.findViewById(R.id.alarm);
+        initViewBackground(rootView); // set background color according to theme
 
+        ListView alarmListView = initAlarmListView(rootView);
+
+        prepareAlarmItemOnClick(alarmListView);
+
+        prepareNewAlarmButtonOnClick(rootView, alarmListView);
+
+        return rootView;
+    }
+
+    private void initViewBackground(View rootView) {
+        View background = rootView.findViewById(R.id.alarm);
         SharedPreferences preference = PreferenceManager.getDefaultSharedPreferences(getActivity());
         int color = preference.getInt("dash_colorkey", getActivity().getResources().getColor(R.color.default_color));
-
         int lightColor = ColorPreference.getLightColor(color, getActivity());
-
         GradientDrawable gd = new GradientDrawable(
                 GradientDrawable.Orientation.TOP_BOTTOM,
                 new int[] {color,lightColor});
         gd.setCornerRadius(0f);
-
         background.setBackgroundDrawable(gd);
+    }
 
+    private ListView initAlarmListView(View rootView) {
+        // get alarms and set alarm list adapter
         List<AlarmDataModel> alarms =  AlarmManagerHelper.getAlarms(getActivity());
-
         final AlarmListAdapter alarmListAdapter = new AlarmListAdapter(getActivity(), alarms);
+        ListView alarmListView = (ListView) rootView.findViewById(R.id.alarm_list);
 
-        final ListView alarmListView = (ListView) rootView.findViewById(R.id.alarm_list);
-
+        // set adaptor
         alarmListView.setAdapter(alarmListAdapter);
+        return alarmListView;
+    }
 
+    private void prepareAlarmItemOnClick(ListView alarmListView) {
+
+        final AlarmListAdapter alarmListAdapter = (AlarmListAdapter)alarmListView.getAdapter();
+
+        // on click listener for alarms
         alarmListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(final AdapterView<?> adapterView, View view, int position, long l) {
 
-                final AlarmDataModel model = (AlarmDataModel) adapterView.getItemAtPosition(position);
+                // prepare view of the dialog to be shown:
                 final Context mContext = getActivity();
-                AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(getActivity());
                 LayoutInflater inflater = getActivity().getLayoutInflater();
                 final View dialog = inflater.inflate(R.layout.alarm_setter_dialog, null);
 
-
-                final TextView alarm_name = (TextView) dialog.findViewById(R.id.alarm_name);
-
-                alarm_name.setText(model.getName());
-
+                // prepare time picker tool
                 final TimePicker tp = (TimePicker) dialog.findViewById(R.id.timePicker);
                 final SharedPreferences preference = PreferenceManager.getDefaultSharedPreferences(mContext);
                 tp.setIs24HourView(preference.getBoolean("24hour_option", true));
+
+                // get model params to set time picker time
+                final AlarmDataModel model = (AlarmDataModel) adapterView.getItemAtPosition(position);
                 tp.setCurrentHour(model.getTimeHour());
                 tp.setCurrentMinute(model.getTimeMinute());
 
+                //set alarm title from model
+                final TextView alarm_name = (TextView) dialog.findViewById(R.id.alarm_name);
+                alarm_name.setText(model.getName());
 
-
-                CheckBox satView = (CheckBox) dialog.findViewById(R.id.alarm_repeat_check);
-
+                // prepare and set repeating checkbox
+                CheckBox repeatCheck = (CheckBox) dialog.findViewById(R.id.alarm_repeat_check);
                 // if the alarm is repeating we should initialize the toggle buttons appropriately
                 if (model.isWeekly()) {
-                    satView.setChecked(true);
+                    repeatCheck.setChecked(true);
                     AlarmFragment.enableToggleRepeat(dialog);
                     updateRepeatingDays(dialog, model);
                 }
 
-                satView.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                setRepeatToggleButtonHandler(dialog);
 
-                    @Override
-                    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                        if (isChecked) {
-                            AlarmFragment.enableToggleRepeat(dialog);
-                        } else {
-                            AlarmFragment.disableToggleRepeat(dialog);
-                        }
-                    }
+                setDailyToggleButtonHandlers(dialog);
 
-                });
-
+                // prepare and get dialog to be shown on click
+                AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(getActivity());
                 dialogBuilder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
@@ -134,18 +144,14 @@ public class AlarmFragment extends Fragment{
                         alarmListAdapter.notifyDataSetChanged();
                     }
                 });
-
                 dialogBuilder.setNegativeButton(R.string.cancel, null);
-
                 dialogBuilder.setView(dialog);
-
                 final AlertDialog dialogToLaunch = dialogBuilder.create();
 
+                // prepare and set delete button
                 ImageButton deleteButton = (ImageButton) dialog.findViewById(R.id.deleteAlarm);
-
                 deleteButton.setVisibility(Button.VISIBLE);
                 deleteButton.setClickable(true);
-
                 deleteButton.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
@@ -155,33 +161,17 @@ public class AlarmFragment extends Fragment{
                     }
                 });
 
+                // show dialog!
                 dialogToLaunch.setCanceledOnTouchOutside(true);
                 dialogToLaunch.requestWindowFeature(Window.FEATURE_NO_TITLE);
                 dialogToLaunch.show();
             }
         });
 
-//        rootView.findViewById(R.id.demo_alarm)
-//                .setOnClickListener(new View.OnClickListener() {
-//                    @Override
-//                    public void onClick(View view) {
-//                        final Calendar c = Calendar.getInstance();
-//                        int hour = c.get(Calendar.HOUR_OF_DAY);
-//                        int minute = c.get(Calendar.MINUTE);
-//                        SharedPreferences preference = PreferenceManager.getDefaultSharedPreferences(getActivity());
-//                        String strRingtonePreference = preference.getString("ringtone_pref", "DEFAULT_SOUND");
-//                        Uri ringtoneUri = Settings.System.DEFAULT_ALARM_ALERT_URI;
-//                        if (!strRingtonePreference.equalsIgnoreCase("DEFAULT_SOUND"))
-//                        {
-//                            ringtoneUri = Uri.parse( strRingtonePreference);
-//                        }
-//                        AlarmDataModel alarmData = new AlarmDataModel("WAAAKE UP!!", hour, minute + 1, ringtoneUri);
-//                        alarmData.setEnabled(true);
-//                        AlarmManagerHelper.createNewAlarm(getActivity(), alarmData);
-//                    }
-//                });
+    }
 
-
+    private void prepareNewAlarmButtonOnClick(View rootView,final ListView alarmListView) {
+        // button to set a new alarm
         rootView.findViewById(R.id.set_alarm_button)
                 .setOnClickListener(new View.OnClickListener() {
                     @Override
@@ -196,21 +186,9 @@ public class AlarmFragment extends Fragment{
                         final SharedPreferences preference = PreferenceManager.getDefaultSharedPreferences(getActivity());
                         tp.setIs24HourView(preference.getBoolean("24hour_option", true));
 
-                        CheckBox satView = (CheckBox)dialog.findViewById(R.id.alarm_repeat_check);
+                        setRepeatToggleButtonHandler(dialog);
 
-                        satView.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-
-                            @Override
-                            public void onCheckedChanged(CompoundButton buttonView,boolean isChecked) {
-                                if (isChecked){
-                                    AlarmFragment.enableToggleRepeat(dialog);
-                                }
-                                else {
-                                    AlarmFragment.disableToggleRepeat(dialog);
-                                }
-                            }
-
-                        });
+                        setDailyToggleButtonHandlers(dialog);
 
                         dialogBuilder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
                             @Override
@@ -260,22 +238,74 @@ public class AlarmFragment extends Fragment{
                                 }
                             }
                         });
-
                         dialogBuilder.setNegativeButton(R.string.cancel, null);
-
-
                         dialogBuilder.setView(dialog);
-
                         final AlertDialog dialogToLaunch = dialogBuilder.create();
 
                         dialogToLaunch.setCanceledOnTouchOutside(true);
                         dialogToLaunch.requestWindowFeature(Window.FEATURE_NO_TITLE);
-
                         dialogToLaunch.show();
                     }
                 });
+    }
 
-        return rootView;
+    private void setRepeatToggleButtonHandler(final View dialog) {
+        CheckBox repeatCheck = (CheckBox)dialog.findViewById(R.id.alarm_repeat_check);
+        repeatCheck.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked)
+                    AlarmFragment.enableToggleRepeat(dialog);
+                else
+                    AlarmFragment.disableToggleRepeat(dialog);
+            }
+        });
+    }
+
+    private void setDailyToggleButtonHandlers(final View dialog) {
+
+        ToggleButton togSunday = ((ToggleButton)dialog.findViewById(R.id.toggle_sunday));
+        ToggleButton togMonday = ((ToggleButton)dialog.findViewById(R.id.toggle_monday));
+        ToggleButton togTuesday = ((ToggleButton)dialog.findViewById(R.id.toggle_tuesday));
+        ToggleButton togWednesday = ((ToggleButton)dialog.findViewById(R.id.toggle_wednesday));
+        ToggleButton togThursday = ((ToggleButton)dialog.findViewById(R.id.toggle_thursday));
+        ToggleButton togFriday = ((ToggleButton)dialog.findViewById(R.id.toggle_friday));
+        ToggleButton togSaturday = ((ToggleButton)dialog.findViewById(R.id.toggle_saturday));
+
+        final CheckBox toggleRepeat = (CheckBox) dialog.findViewById(R.id.alarm_repeat_check);
+
+        CompoundButton.OnCheckedChangeListener toggleButtonClickhandler = new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    toggleRepeat.setChecked(true);
+                } else {
+
+                    ToggleButton togSunday = ((ToggleButton)dialog.findViewById(R.id.toggle_sunday));
+                    ToggleButton togMonday = ((ToggleButton)dialog.findViewById(R.id.toggle_monday));
+                    ToggleButton togTuesday = ((ToggleButton)dialog.findViewById(R.id.toggle_tuesday));
+                    ToggleButton togWednesday = ((ToggleButton)dialog.findViewById(R.id.toggle_wednesday));
+                    ToggleButton togThursday = ((ToggleButton)dialog.findViewById(R.id.toggle_thursday));
+                    ToggleButton togFriday = ((ToggleButton)dialog.findViewById(R.id.toggle_friday));
+                    ToggleButton togSaturday = ((ToggleButton)dialog.findViewById(R.id.toggle_saturday));
+
+                    Boolean noneIsChecked = !togSunday.isChecked() && !togMonday.isChecked() && !togTuesday.isChecked()
+                            && !togWednesday.isChecked() && !togThursday.isChecked() && !togFriday.isChecked() && !togSaturday.isChecked();
+
+                    if (noneIsChecked) // if none is checked now, set repeat to disabled
+                        toggleRepeat.setChecked(false);
+                }
+
+            }
+        };
+
+        togSunday.setOnCheckedChangeListener(toggleButtonClickhandler);
+        togMonday.setOnCheckedChangeListener(toggleButtonClickhandler);
+        togTuesday.setOnCheckedChangeListener(toggleButtonClickhandler);
+        togWednesday.setOnCheckedChangeListener(toggleButtonClickhandler);
+        togThursday.setOnCheckedChangeListener(toggleButtonClickhandler);
+        togFriday.setOnCheckedChangeListener(toggleButtonClickhandler);
+        togSaturday.setOnCheckedChangeListener(toggleButtonClickhandler);
     }
 
     public static void enableToggleRepeat(View dialog) {
@@ -286,27 +316,19 @@ public class AlarmFragment extends Fragment{
         ToggleButton togThursday = ((ToggleButton)dialog.findViewById(R.id.toggle_thursday));
         ToggleButton togFriday = ((ToggleButton)dialog.findViewById(R.id.toggle_friday));
         ToggleButton togSaturday = ((ToggleButton)dialog.findViewById(R.id.toggle_saturday));
-        togSunday.setEnabled(true);
-        togMonday.setEnabled(true);
-        togTuesday.setEnabled(true);
-        togWednesday.setEnabled(true);
-        togThursday.setEnabled(true);
-        togFriday.setEnabled(true);
-        togSaturday.setEnabled(true);
-        togSunday.setClickable(true);
-        togMonday.setClickable(true);
-        togTuesday.setClickable(true);
-        togWednesday.setClickable(true);
-        togThursday.setClickable(true);
-        togFriday.setClickable(true);
-        togSaturday.setClickable(true);
-        togSunday.setChecked(false);
-        togMonday.setChecked(true);
-        togTuesday.setChecked(true);
-        togWednesday.setChecked(true);
-        togThursday.setChecked(true);
-        togFriday.setChecked(true);
-        togSaturday.setChecked(false);
+
+
+        Boolean oneAlreadyChecked = togSunday.isChecked() || togMonday.isChecked() || togTuesday.isChecked()
+                || togWednesday.isChecked() || togThursday.isChecked() || togFriday.isChecked() || togSaturday.isChecked();
+        if (!oneAlreadyChecked) { // if one of them is already checked means this is getting automaticly enabled!
+            togSunday.setChecked(false);
+            togMonday.setChecked(true);
+            togTuesday.setChecked(true);
+            togWednesday.setChecked(true);
+            togThursday.setChecked(true);
+            togFriday.setChecked(true);
+            togSaturday.setChecked(false);
+        }
     }
 
     public static void disableToggleRepeat(View dialog) {
@@ -317,20 +339,7 @@ public class AlarmFragment extends Fragment{
         ToggleButton togThursday = ((ToggleButton)dialog.findViewById(R.id.toggle_thursday));
         ToggleButton togFriday = ((ToggleButton)dialog.findViewById(R.id.toggle_friday));
         ToggleButton togSaturday = ((ToggleButton)dialog.findViewById(R.id.toggle_saturday));
-        togSunday.setEnabled(false);
-        togMonday.setEnabled(false);
-        togTuesday.setEnabled(false);
-        togWednesday.setEnabled(false);
-        togThursday.setEnabled(false);
-        togFriday.setEnabled(false);
-        togSaturday.setEnabled(false);
-        togSunday.setClickable(false);
-        togMonday.setClickable(false);
-        togTuesday.setClickable(false);
-        togWednesday.setClickable(false);
-        togThursday.setClickable(false);
-        togFriday.setClickable(false);
-        togSaturday.setClickable(false);
+
         togSunday.setChecked(false);
         togMonday.setChecked(false);
         togTuesday.setChecked(false);
@@ -341,14 +350,17 @@ public class AlarmFragment extends Fragment{
     }
 
     public static void setRepeatingDays(View dialog, AlarmDataModel alarmData) {
-        alarmData.setWeeklyRepeat(((CheckBox)dialog.findViewById(R.id.alarm_repeat_check)).isChecked());
-        alarmData.setRepeatingDay(0, ((ToggleButton)dialog.findViewById(R.id.toggle_sunday)).isChecked());
-        alarmData.setRepeatingDay(1, ((ToggleButton)dialog.findViewById(R.id.toggle_monday)).isChecked());
-        alarmData.setRepeatingDay(2, ((ToggleButton)dialog.findViewById(R.id.toggle_tuesday)).isChecked());
-        alarmData.setRepeatingDay(3, ((ToggleButton)dialog.findViewById(R.id.toggle_wednesday)).isChecked());
-        alarmData.setRepeatingDay(4, ((ToggleButton)dialog.findViewById(R.id.toggle_thursday)).isChecked());
-        alarmData.setRepeatingDay(5, ((ToggleButton)dialog.findViewById(R.id.toggle_friday)).isChecked());
-        alarmData.setRepeatingDay(6, ((ToggleButton)dialog.findViewById(R.id.toggle_saturday)).isChecked());
+        boolean isRepeating = ((CheckBox)dialog.findViewById(R.id.alarm_repeat_check)).isChecked();
+        alarmData.setWeeklyRepeat(isRepeating);
+        if (isRepeating) { //do not set repeating days for a non repeating alarm.
+            alarmData.setRepeatingDay(0, ((ToggleButton) dialog.findViewById(R.id.toggle_sunday)).isChecked());
+            alarmData.setRepeatingDay(1, ((ToggleButton) dialog.findViewById(R.id.toggle_monday)).isChecked());
+            alarmData.setRepeatingDay(2, ((ToggleButton) dialog.findViewById(R.id.toggle_tuesday)).isChecked());
+            alarmData.setRepeatingDay(3, ((ToggleButton) dialog.findViewById(R.id.toggle_wednesday)).isChecked());
+            alarmData.setRepeatingDay(4, ((ToggleButton) dialog.findViewById(R.id.toggle_thursday)).isChecked());
+            alarmData.setRepeatingDay(5, ((ToggleButton) dialog.findViewById(R.id.toggle_friday)).isChecked());
+            alarmData.setRepeatingDay(6, ((ToggleButton) dialog.findViewById(R.id.toggle_saturday)).isChecked());
+        }
     }
 
     public static void updateRepeatingDays(View dialog, AlarmDataModel alarmData) {
@@ -360,6 +372,5 @@ public class AlarmFragment extends Fragment{
         ((ToggleButton)dialog.findViewById(R.id.toggle_friday)).setChecked(alarmData.getRepeatingDay(5));
         ((ToggleButton)dialog.findViewById(R.id.toggle_saturday)).setChecked(alarmData.getRepeatingDay(6));
     }
-
 
 }
